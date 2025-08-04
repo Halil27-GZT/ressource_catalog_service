f26d6ef9329dcad0d5aea37dffa07a1d3153551c
Halil27-GZT <tuerkseven@vodafone.de>
2025-08-04 12:59:20 +0200

    RC-003: Implementeces/id endpoint.

--- server.js
+++ server.js
@@ -20,10 +20,31 @@ app.get('/resources', (req, res) => {
         const resources = JSON.parse(data);
         res.json(resources);
     } catch (error) {
-        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Daten'})
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Daten' })
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.get('/resources/:id', (req, res) => {
+    try {
+        const resourceId = req.params.id;
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+        const resource = resources.find(r => r.id === resourceId);
+
+        if (resource) {
+            res.json(resource);
+        } else {
+            res.status(404).json({ error: `Ressource mit ID ${resourceId} nicht gefunden.` })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Ressource' });
+    }
+
 });
+
+
+
+    app.listen(port, () => {
+        console.log(`Server is running at http://localhost:${port}`);
+});
+
